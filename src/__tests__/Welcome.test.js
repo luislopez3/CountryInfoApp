02d680c45c33724f9903d1602f96d90d4c4fa416
Luis Lopez <llopez1518@gmail.com>
2021-06-27 10:06:58 -0600

    Some Cleanup

--- src/__tests__/Welcome.test.js
+++ src/__tests__/Welcome.test.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { create } from 'react-test-renderer';
-import { MemoryRouter } from 'react-router';
-import Welcome from '../Welcome.js';
+import React from "react";
+import { create } from "react-test-renderer";
+import { MemoryRouter } from "react-router";
+import Welcome from "../Welcome.js";
 
 describe("Show Details component", () => {
-    test("Matches the snapshot", () => {
-        const wrapper = create(
-        <MemoryRouter ><Welcome /></MemoryRouter>
-        );
-        expect(wrapper.toJSON()).toMatchSnapshot();
-    });
+  test("Matches the snapshot", () => {
+    const wrapper = create(
+      <MemoryRouter>
+        <Welcome />
+      </MemoryRouter>
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
 });
