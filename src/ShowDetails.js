1457255cc363ed0254d567c9433e53989307b9ca
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2025-03-11 12:27:14 -0600

    revise country name to match object render and add useEffect for scroll into view render

--- src/ShowDetails.js
+++ src/ShowDetails.js
@@ -7,22 +7,25 @@ export default function ShowDetails() {
   const { country } = useParams();
   const details = useRef(null);
 
+  // Fetch country information when the country param changes
   useEffect(() => {
     countryInfo().then((response) => {
       setCountryDetails(response);
-      showDetails();
     });
   }, [country]);
 
-  const showDetails = () => {
-    details.current.scrollIntoView({ behavior: "smooth" });
-  };
+  // Trigger scrollIntoView after the countryDetails are set and rendered
+  useEffect(() => {
+    if (details.current) {
+      details.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [countryDetails]);
 
   return (
     <>
       <h1>Details</h1>
       {countryDetails
-        .filter((item) => item.name === country)
+        .filter((item) => item.name.common === country)
         .map(
           (
             {
@@ -39,20 +42,26 @@ export default function ShowDetails() {
           ) => {
             return (
               <div ref={details} key={index}>
-                <img src={flag} alt={`${name}'s flag`} />
+                <img src={flag} alt={`${name.common}'s flag`} />
                 <h3 className="country-info">
-                  Country Name: <h4 className="country-details">{name.common}</h4>
+                  Country Name:{" "}
+                  <h4 className="country-details">{name.common}</h4>
                 </h3>
                 <p className="country-info">
-                  Capital City: <p className="country-details">{capital}</p>
+                  Capital City:{" "}
+                  <p className="country-details">{capital}</p>
                 </p>
                 <p className="country-info">
                   Currency:{" "}
-                  <p className="country-details">{currencies[0].name}</p>
+                  <p className="country-details">
+                    {currencies && Object.values(currencies)[0].name}
+                  </p>
                 </p>
                 <p className="country-info">
                   Most Popular Language:{" "}
-                  <p className="country-details">{languages[0].name}</p>
+                  <p className="country-details">
+                    {languages && Object.values(languages)[0]}
+                  </p>
                 </p>
                 <p className="country-info">
                   Population:{" "}
@@ -64,10 +73,7 @@ export default function ShowDetails() {
                   Area:{" "}
                   <p className="country-details">
                     {new Intl.NumberFormat().format(area)} km² (
-                    {new Intl.NumberFormat().format(
-                      Math.round(area * 0.621371)
-                    )}{" "}
-                    mi²)
+                    {new Intl.NumberFormat().format(Math.round(area * 0.621371))} mi²)
                   </p>
                 </p>
                 <p className="country-info">
@@ -79,8 +85,7 @@ export default function ShowDetails() {
                     /{" "}
                     {new Intl.NumberFormat("en-IN", {
                       maximumSignificantDigits: 2,
-                    }).format(latlng[1])}{" "}
-                    (Coordinates in Decimal Degrees)
+                    }).format(latlng[1])} (Coordinates in Decimal Degrees)
                   </p>
                 </p>
               </div>
